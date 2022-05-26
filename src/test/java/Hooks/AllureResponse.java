8077ceef3e3f6ca6888f75ab65ffb0cf8658e58c
oltark <o.tarkhanov@gmail.com>
2022-05-26 15:29:39 +0300

    +allure

---
+++ src/test/java/Hooks/AllureResponse.java
@@ -0,0 +1,17 @@
+package Hooks;
+
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.BeforeAll;
+
+public class AllureResponse {
+
+    @BeforeAll
+    public static void allureSubThread() {
+        String listenerName = "allureSelenide";
+
+        if(!(SelenideLogger.hasListener(listenerName)))
+            SelenideLogger.addListener(listenerName, new AllureSelenide().screenshots(true).savePageSource(false));
+
+    }
+}
--- src/test/java/RunnerTest.java
+++ src/test/java/RunnerTest.java
@@ -1,13 +1,21 @@
 import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
+import io.qameta.allure.restassured.AllureRestAssured;
+import io.restassured.RestAssured;
+import org.junit.BeforeClass;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
         features = "src/test/resources/feature",
-        glue = {"StepDefinition", "Hooks"},
+        plugin = {"pretty", "io.qameta.allure.cucumber6jvm.AllureCucumber6Jvm", "json:target/cucumber.json"},
+        glue = {"StepDefinition", "AllureResponse", "Hooks"},
         tags = "@TEST"
 )
+class RunnerTest {
+    @BeforeClass
+    public static void before() {
+        RestAssured.filters(new AllureRestAssured()) ;
+    }
 
-public class RunnerTest {
 }
